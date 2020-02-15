f1631116a08315c5df593aafe18f70a714b412d5
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-15 18:50:25 -0300

    C - HelloWord

---
+++ 001 - HelloWord/HelloWord.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h> //pausa
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//Função principal do programa
+int main(int argc, char *argv[]) {
+	
+	//Imprime na tela
+	printf("Hello Word\n");
+	printf("Relembrando");
+	
+	//Pausa o programa após executar
+	system("pause");
+	
+	//retorna o valor 0
+	return 0;
+}
