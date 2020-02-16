01b889dd6f753b95beee7224d6fc31d7fe0a868b
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-16 13:39:13 -0300

    CondicoesChar

---
+++ 008 - CondicoesChar/008 - CondicoesChar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char letra = 'x';
+	
+	if(letra == 'x')
+	{
+		printf("\n A letra eh x");
+	}
+	
+	printf("\n Codigo da letra = %d", letra);
+	
+	if(letra == 120)
+	{
+		printf("\n A letra eh x");
+	}
+	else
+	{
+		
+	}
+	return 0;
+}
