97fda925aec8831d73f2b297f0c01c968acf5fb4
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-22 21:51:17 -0300

    LacoDeRepeticao

---
+++ 012 - LaçoDeRepeticao(WHILE)/012 - while.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a = 1;
+	int b = 10;
+	
+	while(a <= 10)
+	{
+		printf("\n %d",a);
+		a++;
+	}
+	
+	while(b >= 1)
+	{
+		printf("\n %d", b);
+		b--;
+	}
+	
+	
+	return 0;
+}
