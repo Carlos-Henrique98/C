dfb2c789876c6ef681da60d1308a5ff0c6b84308
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-22 22:08:25 -0300

    LacoDeRepeticao

---
+++ 013 - LacoDeRepeticao(DOWHILE)/dowhile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a =  1;
+	
+	do{
+		
+		printf("\n %d",a);
+		
+		a++;
+			
+	}while(a <= 10);
+	return 0;
+}
